ee8f02699be89e295804b9346bfca5c593439139
Aellun <okeelo.k.otieno@gmail.com>
2024-02-07 10:22:46 +0300

    updated the place_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Module docstring: Manages the creation, retrieval, and modification
-of Review objects via RESTful endpoints.
-This module implements routes for handling review-related operations
-such as listing reviews for a place, creating a new review, retrieving
-a review by ID, updating a review, and deleting a review.
+Module doctstring: This module provides the implementation for the review
+in the AirBnB clone API
+The module also allows for the management of Review objects,
+including operations such as listing reviews for a place,
+creating new reviews, retrieving a review by ID,
+updating a review, and deleting a review.
+'''
+
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
@@ -15,10 +18,9 @@ from models.review import Review
                  strict_slashes=False)
 def reviews_by_place(place_id):
     """
-    '''Endpoint for retrieving all reviews
-    associated with a specified place.
-    Returns a JSON array of all reviews for the specified place.
-    If the place is not found, a  404 error is returned.
+    Handler method for retrieving all reviews associated with a specific place.
+    Returns a JSON array of all reviews for the specified place ID
+    If the place does not exist, a  404 error is returned.
     """
     review_list = []
     place_obj = storage.get("Place", str(place_id))
@@ -36,13 +38,13 @@ def reviews_by_place(place_id):
                  strict_slashes=False)
 def review_create(place_id):
     """
-    '''Endpoint method for creating a new review for a given place.
-    Accepts a JSON payload with the review details,
-    including the user_id and text.
-    If the place or user is not found,
-    or the required fields are missing,
-    a  400 or  404 error is returned."""
-
+    Handler function for creating a new review for a specific place.
+    Requires a JSON payload with 'user_id' and 'text' fields.
+    If the place or user does not exist, or if the JSON payload is missing
+    required fields, a  400 or  404 error is returned.
+    On successful creation, returns the newly created review JSON object
+    with a  201 status code.
+    """
     review_json = request.get_json(silent=True)
     if review_json is None:
         abort(400, 'Not a JSON')
@@ -69,10 +71,7 @@ def review_create(place_id):
                  strict_slashes=False)
 def review_by_id(review_id):
     """
-    Endpoint method for retrieving
-    a single review by its unique identifier
-    Returns the requested review as a JSON object.
-    If the review is not found, a  404 error is returned.
+    handler method that gets a specific Review object by ID
         :param review_id: place object id
         :return: review obj with the specified id or error
     """
@@ -89,11 +88,7 @@ def review_by_id(review_id):
                  strict_slashes=False)
 def review_put(review_id):
     """
-    Endpoint method for updating an existing
-    review by its unique identifier.
-    Accepts a JSON payload with the updated review details.
-    If the review is not found, or the JSON is malformed,
-    a  400 or  404 error is returned.
+    handler method that updates specific Review object by ID
         :param review_id: Review object ID
         :return: Review object and 200 on success, or 400 or 404 on failure
     """
@@ -121,11 +116,9 @@ def review_put(review_id):
                  strict_slashes=False)
 def review_delete_by_id(review_id):
     """
-    Endpoint method for deleting an existing review
-    by its unique identifier.
-    Deletes the specified review from the data store.
-        :param : Review object id
-        :return: empty dict with 200 or 404 if not found
+    deletes Review by id
+    :param : Review object id
+    :return: empty dict with 200 or 404 if not found
     """
 
     fetched_obj = storage.get("Review", str(review_id))
